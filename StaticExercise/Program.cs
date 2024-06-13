26962751a411e36afad9e4071a34431696dba54f
Derrick <Avirajoesph@yahoo.com>
2024-06-13 13:43:10 -0500

    fixed project

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(TempConverter.FarenheittoCelsius(5.8));
+
+            Console.WriteLine(TempConverter.CelsiusToFahrenheit(4.6));
+            
+
         }
+
     }
-}
+}       
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    static  class TempConverter
+    {
+        public static double FarenheittoCelsius(double numbers) 
+        {
+
+            return (numbers - 32) / 1.8;
+
+
+
+        }
+        public static double CelsiusToFahrenheit(double numbers) 
+        {
+
+
+            return numbers * 1.8 + 32;
+
+
+        }
+
+
+    }
+}
